9b430e575a17b5f07d3728e2f3bae32fc668082d
Dalaio1002 <dalai.dev1002@gmail.com>
2024-10-08 23:48:50 +0800

    made the header ui

---
+++ src/components/atom/cards/wallet-card.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Image from 'next/image'
+
+interface cardProps {
+    image: any;
+    title: string;
+}
+
+const WalletCard: React.FC<cardProps> = ({ image, title }) => {
+  return (
+    <div className='flex flex-row w-full gap-4 bg-white8 p-3 items-center rounded-2xl'>
+        <Image src={image} alt='image' width={40} height={40} sizes='100%' className='w-10 h-10 rounded-lg'/>
+        <span className='text-neutral50 text-lg2 font-bold'>{title}</span>
+    </div>
+  )
+}
+
+export default WalletCard
--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -19,14 +19,14 @@ const SelectTrigger = React.forwardRef<
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      "flex h-10 w-full items-center text-start justify-between text-neutral50 font-semibold text-lg rounded-md border border-input bg-neutral400 px-3 py-2 ring-offset-neutral400 placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring ring-transparent focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
+      "flex h-10 w-full items-center text-start justify-between text-neutral50 font-semibold text-lg rounded-md border border-input bg-neutral400 px-3 py-2 placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring ring-transparent focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
       className
     )}
     {...props}
   >
     {children}
     <SelectPrimitive.Icon asChild>
-      <ChevronDown className="h-7 w-7 text-neutral50" />
+      <ChevronDown className="h-5 w-5 text-neutral50" />
     </SelectPrimitive.Icon>
   </SelectPrimitive.Trigger>
 ))
@@ -75,7 +75,7 @@ const SelectContent = React.forwardRef<
     <SelectPrimitive.Content
       ref={ref}
       className={cn(
-        "relative z-50 h-[208px] pl-8 pt-4 overflow-hidden text-neutral text-center rounded-md border bg-neutral600  bg-popover text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 ring-transparent",
+        "relative z-50 pt-4 overflow-hidden text-neutral text-center rounded-md border bg-neutral600  bg-popover text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 ring-transparent",
         position === "popper" &&
           "data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1",
         className
@@ -88,7 +88,7 @@ const SelectContent = React.forwardRef<
         className={cn(
           "p-1",
           position === "popper" &&
-            "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]"
+            "h-[var(--radix-select-trigger-height)]"
         )}
       >
         {children}
@@ -118,7 +118,7 @@ const SelectItem = React.forwardRef<
   <SelectPrimitive.Item
     ref={ref}
     className={cn(
-      "relative flex h-10 w-full cursor-default text-neutral50 select-none  hover:bg-neutral500 hover:w-[190px]  font-semibold items-center rounded-lg pt-1 pr-5 pb-2 pl-8 text-md2 outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+      "relative flex h-10 cursor-default text-neutral50 select-none hover:bg-neutral500 font-semibold rounded-lg p-2 text-md2 outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
       className
     )}
     {...props}
