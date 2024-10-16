8af17fbd65df9951887773bd9f4eb02cccbdf95d
Shaazgai <bzao.hover@gmail.com>
2024-10-16 20:56:50 +0800

    collection page card hoverButton

--- src/components/atom/cards/collectionDetailCard.tsx
+++ src/components/atom/cards/collectionDetailCard.tsx
@@ -24,15 +24,20 @@ export default function ColDetailCard({ data }: { data: DetailCard }) {
           <p className="text-neutral200 font-medium text-md pt-2">
             {data.title}
           </p>
-          <p className="py-1 text-lg2 text-neutral50 font-bold border-b border-neutral400 pb-4">
+          <p className="py-1 text-lg2 text-neutral50 font-bold pb-4">
             {data.title}
           </p>
-          <div className="flex justify-between py-4">
-            <p className="text-neutral200 font-medium text-md">Price</p>
-            <p className="text-neutral50">
-              {data.price}
-              <span className="ml-1">BTC</span>
-            </p>
+          <div className="relative group h-10 w-[248px] hover:border-hidden border-t border-neutral400">
+            <div className="flex justify-between py-4 group-hover:hidden">
+              <p className="text-neutral200 font-medium text-md">Price</p>
+              <p className="text-neutral50">
+                {data.price}
+                <span className="ml-1">BTC</span>
+              </p>
+            </div>
+            <div className="group-hover:block hidden text-center transition-opacity cursor-pointer duration-1000 ease-in-out w-[248px] h-10 absolute top-0 left-0 text-white bg-white4 pt-2 pr-5 pb-2 pl-5 rounded-lg">
+              Buy now
+            </div>
           </div>
         </div>
         {/* <HoverCard /> */}
